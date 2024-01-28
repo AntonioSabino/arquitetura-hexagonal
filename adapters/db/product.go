600ce9a3f4ff055ed6c0e1c2869ffc642ae7b33c
Antonio Sabino <aslimajr@outlook.com>
2024-01-27 21:33:11 -0300

    implentando método Save

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -31,3 +31,53 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 
 	return &product, nil
 }
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("insert into products(id, name, status, price) values(?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetStatus(), product.GetPrice())
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("update products set name = ?, status = ?, price = ? where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.GetName(), product.GetStatus(), product.GetPrice(), product.GetID())
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+
+	p.db.QueryRow("select id from products where id = ?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
+}
